f9244b3cce11b2877b6f49352b8e5a53f7a94bd7
harshit391 <harshitsingla1761@gmail.com>
2023-09-07 10:10:40 +0530

    Notes Updated 6 September

--- C++/5_September/Encapsulation.cpp
+++ C++/5_September/Encapsulation.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 
+// It is the mechanism of wrapping the data and the code that operates on that data into a single unit. 
+// This makes the data accessible only through the code in the unit, 
+// thus preventing unauthorized access.
 
 using namespace std;
 
-
+// For example there is class about a car
 class Car{
+    // We have to keep the price private so that no one should have power to change it
+    // So we kept price in private block which make it accessible only in the Car class 
+
 private:
     int price = 100;
 
+    // But like the seller company should have access to the discount So we create seperate functions to set discount in public block
+    // This make the functions declared in public block to be accessible outside the Car class
 public:
+    // So in public we can just set price to whatever we want as per discount by seller
     void set_price(int x)
     {
         if (x>50)
@@ -21,6 +30,11 @@ public:
         }
     }
 
+    // And to get the manipulated price we make a seperate function
+
+    // Now as you can see we can't directly manipulate the private price but we can return its value to know whats the price
+    // This makes it only authorised to a specific person and unauthorized person only get to know the price
+    // They can't change it as it is in private block
     int get_price()
     {
         return price;
@@ -29,10 +43,13 @@ public:
 
 int main()
 {
+    // Initializing a Car object name BMW
     Car BMW;
-    // BMW.price = 0;
+    // BMW.price = 0; --> Now if we keep price in public someone will manipulate it easily which lead to loss of Company 
 
-    BMW.set_price(90);
+    BMW.set_price(90); // Yeah but seller has power to set discount so he/she can use the set price function to give the discount as per they wish
 
+    // At last to show the price to user we use get price function in public which only show the price
+    // It doesn't give any access to manipulate the prize
     printf("%d\n",BMW.get_price());
 }
--- C++/5_September/Overloading.cpp
+++ C++/5_September/Overloading.cpp
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+// Overloading is a C++ programming feature that allows us to have more than one function having same name but different parameter list. 
+// The parameter list, in this case, includes the data type and sequence of the parameters.
+
+// For example
+
+// There is a function of sum which add two integer
+// Let this function be sum1
 void sum(int a, int b)
 {
     printf("%d\n", a+b);
 }
 
+// But there is a function of sum which add one integer and other float value
+// This is actually called Overloading
+// Let this function be sum2
 void sum(int a, float b)
 {
     printf("%.2f\n", a+b);
@@ -14,10 +24,14 @@ int main()
 {
     int a = 10,b=20;
 
+    // When this function called sum1 function got allocated in stack as we are passing two integers
+    // So it automatically detects which function is actually been called
     sum(a,b);
 
     float c = 1.69;
 
+    // Where when this function called sum2 function got allocated in stack as we are passing one integer and other float value
+    // So it automatically detect the change and switch to another function 
     sum(a,c);
 
 
