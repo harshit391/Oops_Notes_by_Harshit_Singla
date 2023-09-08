4fa5bb7ba56d84b00bf6de608243d5a7cb85a775
harshit391 <harshitsingla1761@gmail.com>
2023-09-08 18:40:50 +0530

    Postfix Evaluation Done

--- C/1_September/InfixToPostFix_Evaluation.c
+++ C/1_September/InfixToPostFix_Evaluation.c
@@ -21,6 +21,9 @@ int getPrecedence(char ch) {
         return 0;
 }
 
+// Return if the character is alphabet, digit or an operator
+
+// If operator return false otherwise true
 int alphaNum(char ch){
 	if(ch >= 'a' && ch <= 'z') return 1;
 	if(ch >= 'A' && ch <= 'Z') return 1;
@@ -28,24 +31,46 @@ int alphaNum(char ch){
 	return 0;
 }
 
+// Main Conversion of Infix to Postfix
 void infixToPostfix(char infix[], char postfix[]) {
+
+    // Implementing Stack using array
     char stack[MAX_SIZE];
+
+    // initializing a stack index as -1 
     int top = -1;
     int i, j = 0;
 
+    //  Untill the current character is termination character
     for (i = 0; infix[i] != '\0'; i++) {
+
+        // If current character is a alphabet or digit append it in postfix
         if (alphaNum(infix[i])) {
             postfix[j++] = infix[i];
-        } else if (infix[i] == '(') {
+        } 
+        
+        // If we encounter a opening bracket append it in stack
+        else if (infix[i] == '(') {
             stack[++top] = infix[i];
-        } else if (infix[i] == ')') {
+        } 
+        
+        // If comes a closing bracket untill we get opening bracket from stack 
+        else if (infix[i] == ')') {
+
+            // append the operators in postfix
             while (top >= 0 && stack[top] != '(') {
                 postfix[j++] = stack[top--];
             }
+
+            // If stack is not empty and there is opening bracket at the top of stack remove it 
             if (top >= 0 && stack[top] == '(') {
                 top--;
             }
-        } else if (isOperator(infix[i])) {
+        }
+        // But if the character is operator and operator at top of stack has high precedence than current operator
+        // Remove that operator from stack and add it to postfix 
+        // At Last add current operator in stack 
+        else if (isOperator(infix[i])) {
             while (top >= 0 && getPrecedence(stack[top]) >= getPrecedence(infix[i])) {
                 postfix[j++] = stack[top--];
             }
@@ -53,26 +78,36 @@ void infixToPostfix(char infix[], char postfix[]) {
         }
     }
 
+    // If stack in not empty yet add all remaining elements in stack to postfix
     while (top >= 0) {
         postfix[j++] = stack[top--];
     }
 
+    // Index where our postfix got end set termination character to that index to end our string
     postfix[j] = '\0';
 }
 
+// Main POST FIX EVALUATION 
 int evaluatePostfix(char postfix[]){
+    // Initialize Stack with a top index of -1 denoting empty stack
     int stack[MAX_SIZE];
     int top = -1;
     int i,operand1,operand2,result;
 
+    // While we encounter termination character in postfix 
     for(i = 0;postfix[i]!='\0';i++){
+        // If character is digit
+        // Convert it to integer form and append it in stack 
         if(alphaNum(postfix[i])){
             stack[++top] = postfix[i] - '0';
         }
+
+        // If operator remove two elements from top store it in two different variables
         else if(isOperator(postfix[i])){
             operand2 = stack[top--];
             operand1 = stack[top--];
 
+            // And perform operation on both values according to operator
             switch (postfix[i]) {
                 case '+':
                     result = operand1 + operand2;
@@ -88,14 +123,18 @@ int evaluatePostfix(char postfix[]){
                     break;
                 case '^':
                     result = 1;
+                    // Basically inbuilt pow function doesn't gives desired values that's why loop is made to calculate power
                     for(int j = 0; j < operand2; j++) {
                         result *= operand1;
                     }
                     break;
             }
+            // At end append the result of two values in stack
             stack[++top] = result;
         }
     }
+
+    // Return stack top element
     return stack[0];
 }
 
