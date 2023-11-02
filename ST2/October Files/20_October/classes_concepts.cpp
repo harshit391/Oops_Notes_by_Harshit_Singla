1f8614f626f82378923b44c8d9affbab9f127036
harshit391 <harshitsingla1761@gmail.com>
2023-11-02 10:00:00 +0530

    Notes of 20 October Added

--- ST2/October Files/20_October/classes_concepts.cpp
+++ ST2/October Files/20_October/classes_concepts.cpp
@@ -2,6 +2,12 @@
 // Created by DELL on 20/10/2023.
 //
 
+/*
+ * If we have to talk about Passing Objects as a parameters
+ *
+ * Then We have three ways in which we can pass it
+ */
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -21,31 +27,58 @@ public:
     }
 };
 
+/*
+ * Like Here if we will pass the object simply as a parameter
+ * A copy of that object is created which doesn't apply any change to object
+ */
 void change1(Employee e) {
     e.id = 391;
     strcpy(e.name, "Singla");
 }
 
+/*
+ * Now we are passing the address of object
+ * Which actually says that we are modifying the object inplace
+ * without creating a copy of it
+ */
 void change2(Employee& e) {
     e.id = 393;
     strcpy(e.name, "Harshpreet");
 }
 
+/*
+ * Same as passing address
+ * IN this we are passing Reference to the object that where it is stored in memory
+ * So we can go there and change the value inside the object inplace
+ */
 void change3(Employee * e) {
     (*e).id = 386;
     strcpy((*e).name, "Behal");
 }
 
+/*
+ * DIFFERENCE BETWEEN PASSING ADDRESS AND REFERENCE
+ *
+ * When Passing Reference we have to de reference it to access their values
+ * In Address it is not required
+ * We can access it normally
+ */
+
 int main() {
     Employee a1("Kukreja",386);
     a1.display();
 
+    // It will print Kukreja , 386 values without changing
     change1(a1);
     a1.display();
 
+    // This will change the values in place
+    // just we have to pass the object normally
     change2(a1);
     a1.display();
 
+    // This will change the value in place
+    // But we have to pass the address of object as a reference
     change3(&a1);
     a1.display();
 
--- ST2/October Files/20_October/return_Object.cpp
+++ ST2/October Files/20_October/return_Object.cpp
@@ -2,10 +2,17 @@
 // Created by DELL on 20/10/2023.
 //
 
+/*
+ * We always study functions that they can return int , float, double, arr
+ *
+ * Just Imagine a Function return an Object
+ */
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
+// There is a Complex class to display Complex Numbers
 class Complex {
 private:
     int x;
@@ -24,16 +31,25 @@ public:
     friend Complex add(Complex, Complex);
 };
 
+// And there is friend function of return type Complex Object which add two Complex numbers
 Complex add(Complex c1, Complex c2) {
     int new_real = c1.x + c2.x;
     int new_i = c1.y + c2.y;
+
+    // There we created a in scope ans object which include added real and imaginary values
+
+    // It will create a new object with these new values
     Complex ans(new_real, new_i);
+
+    // And we are returning that particular object
     return ans;
 }
 
 int main() {
     Complex a(1,2);
     Complex b(3,4);
+
+    // There we are storing the returned object in another Complex class object
     Complex add_ans = add(a,b);
     add_ans.display();
 }
