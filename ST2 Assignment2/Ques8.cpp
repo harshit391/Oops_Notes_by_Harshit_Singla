a8df59e7816ac89652561d48d21d60ae4e00ebe9
harshit391 <harshitsingla1761@gmail.com>
2023-11-04 09:21:49 +0530

    Solutions of Assignment Added

--- ST2 Assignment2/Ques8.cpp
+++ ST2 Assignment2/Ques8.cpp
@@ -12,3 +12,52 @@ assumed that elements in both arrays are distinct.
 Input: arr1[] = {11, 1, 13, 21, 3, 7}, arr2[] = {11, 3, 7, 1}
 Output: arr2[] is a subset of arr1[]
  */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int * arr = new int[n];
+
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+
+    int m;
+    cin >> m;
+
+    int * arr2 = new int[m];
+
+    for (int i=0;i<m;i++) {
+        cin >> arr2[i];
+    }
+
+    int i=0,j=0;
+
+    sort(arr, arr+n);
+    sort(arr2, arr2+m);
+
+    while (i < n && j < m) {
+        if (arr[i] < arr2[j]) {
+            i++;
+        } else if (arr2[j] == arr[i]) {
+            i++;
+            j++;
+        } else {
+            cout << "false" << endl;
+            return 0;
+        }
+    }
+
+    if (j==m) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
